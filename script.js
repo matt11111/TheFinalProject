56f0b9511cf53f866725c8a0cb02fa8aeed6e354
Not Matthew Iorio <44502882+matt11111@users.noreply.github.com>
2019-05-16 09:19:31 -0400

    Update script.js
    explained functions

--- script.js
+++ script.js
@@ -8,18 +8,21 @@ const rock_div = document.getElementById("r")
 const paper_div = document.getElementById("p")
 const scissors_div = document.getElementById("s")
 
+//this function makes the computer choose an option
 function getComputerChoice() {
 const choices = ['r', 'p', 's'];
 const randomNumber =Math.floor(Math.random() * 3);
 return choices[randomNumber];
 }
 
+//when click on object it projects the name of the object
 function convertToWord(letter) {
 if (letter === "r") return "Rock"
 if (letter === "p") return "Paper"
 return "Scissors";
 }
 
+//what it says when the user wins
 function win(userChoice, computerChoice) {
 userScore++;
 userScore_span.innerHTML = userScore;
@@ -27,6 +30,7 @@ computerScore_span.innerHTML = computerScore;
 result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + ". YOU WIN!";
 }
 
+//what it says when the function loses
 function lose(userChoice, computerChoice) {
 computerScore++;
 userScore_span.innerHTML = userScore;
@@ -34,10 +38,12 @@ computerScore_span.innerHTML = computerScore;
 result_p.innerHTML = convertToWord(computerChoice) + " beats " + convertToWord(userChoice) + ". YOU LOSE!";
 }
 
+//when it is a tie
 function draw(userChoice, computerChoice) {
 result_p.innerHTML = convertToWord(computerChoice) + " equals " + convertToWord(userChoice) + ". DRAW!";
 }
 
+//scenarios that could happen during the game
 function game(userChoice) {
 const computerChoice = getComputerChoice();
 switch (userChoice + computerChoice) {
@@ -59,6 +65,7 @@ break;
 }
 }
 
+//register input from a click
 function main() {
 rock_div.addEventListener('click', function() {
   game("r");
